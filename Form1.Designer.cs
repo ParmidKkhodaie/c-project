77688973decda02fc5db11fc6f903655f9de9143
parmida khodaie <khodaieparmida@gmail.com>
2025-06-28 16:56:49 +0330

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace shoping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.emptySpaceItem1 = new DevExpress.XtraLayout.EmptySpaceItem();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(10, 10);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(950, 272, 650, 400);
+            this.layoutControl1.OptionsView.RightToLeftMirroringApplied = true;
+            this.layoutControl1.Padding = new System.Windows.Forms.Padding(10);
+            this.layoutControl1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.layoutControl1.Root = this.Root;
+            this.layoutControl1.Size = new System.Drawing.Size(1167, 523);
+            this.layoutControl1.TabIndex = 0;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // Root
+            // 
+            this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.Root.GroupBordersVisible = false;
+            this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.emptySpaceItem1});
+            this.Root.Name = "Root";
+            this.Root.Padding = new DevExpress.XtraLayout.Utils.Padding(9, 9, 9, 9);
+            this.Root.Size = new System.Drawing.Size(1167, 523);
+            this.Root.TextVisible = false;
+            // 
+            // emptySpaceItem1
+            // 
+            this.emptySpaceItem1.AllowHotTrack = false;
+            this.emptySpaceItem1.Location = new System.Drawing.Point(0, 0);
+            this.emptySpaceItem1.Name = "emptySpaceItem1";
+            this.emptySpaceItem1.Padding = new DevExpress.XtraLayout.Utils.Padding(10, 10, 10, 10);
+            this.emptySpaceItem1.Size = new System.Drawing.Size(1149, 505);
+            this.emptySpaceItem1.TextSize = new System.Drawing.Size(0, 0);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1187, 543);
+            this.Controls.Add(this.layoutControl1);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Text = "فروشگاه";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup Root;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DXApplication15
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HADAFEntities db = new HADAFEntities();
+            
+            tblMoshtari m1 = new tblMoshtari                 
+
+            {
+                FullName = "parmida",
+                Famil = "khodaie",
+                Address = "ahvaz",
+                Jensiat = 1,
+                Tell = "0916283887",
+                
+            };
+
+            db.tblMoshtari.Add(m1);
+
+            db.saveChanges();
+
+            txtTedad.Text = "";
+            txtTozihat.Text = "";
+                    
+
+            //MessageBox.Show("+m1.Name+""در پایگاه داده ذخیره شد")
+        }
+
+        //private void txtbtnSave_Click(object sender, eventArgs e)
+        // {
+
+
+        // }
+
+        // private void textEdit1_EditValueChanged_1(object sender, EventArgs e)
+        // {
+
+    }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HadafEntities db = new HadafEntities();
+
+            TblMoshtari m1 = new TblMoshtari()
+            {
+
+        //private void txtbtnSave_Click(object sender, EventArgs e)
+        // {
+
+        // }
+
+        //  private void textEdet1_EditValuechanged_1(object sender, EventArgs e)
+        // {
+
+    }
+
+        private void spinEdit1_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void memoEdit1_EditValueChanged(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void spinEdit1_EditValueChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textEdit2_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void simpleButton1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
